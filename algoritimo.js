8624e47507d716f38cea9663a960938dea471dfb
francivaldonlima <francivaldonlima@gmail.com>
2024-03-16 17:05:49 -0300

    começando com nova atualização

---
+++ algoritimo.js
@@ -0,0 +1,7 @@
+const nota1 = parseInt(require('Digite a primeira nota'));
+const nota2 = parseInt(require('Digite a segunda nota'));
+const nota3 = parseInt(require('Digite a terceira nota'));
+
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(`A média é ${media}`);
+
---
+++ algoritimo2.js
@@ -0,0 +1,2 @@
+console.log('Digite a primeira nota');
+const nota1 = prompt('Digite a primeira nota');
